e39ba33531f0762fb5d3a0953f93e0d13bc61378
berkanozm <ozmberkan@gmail.com>
2024-03-11 16:30:25 +0300

    New folder added

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ViteEnvLoader from 'vite-env-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),ViteEnvLoader({ envFile: '.env', watchFile: true})]
+  plugins: [react()],
 })
